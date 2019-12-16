ae959489b8fc5d0181bbedb7494ed6743048f19e
chris tasatzis <christasa@hotmail.com>
2019-12-16 16:49:44 +0200

    remove days

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const methodOverride = require('method-override')
 const passport = require('passport')
 const indexRouter = require('./routes/index')
 const Users = require('./models/user')
+const schedule = require('node-schedule');
 
 
 app.set('view engine', 'ejs')
@@ -29,14 +30,29 @@ initializePassport(
   async id => await Users.find({_id: id})
 )
 
+//Database connection
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://user:nJNwnqOmJoxKlQMV@cluster0-2srjd.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
+//Daily Task to remove one day from the users
+var rule = new schedule.RecurrenceRule();
+rule.hour = 0;
+rule.minute = 0;
+rule.second = 0;
+
+var job = schedule.scheduleJob(rule, function(){
+  console.log('kek')
+  Users.updateMany({timedTicket: { $gt: 0} }, { $inc: {timedTicket: -1} }, function (err) {
+    if (err) 
+      return console.error(err) })
+});
+
+
 app.use('/', indexRouter)
-app.use('/login', indexRouter)
+
 
 
 app.listen(process.env.PORT || 3000)
